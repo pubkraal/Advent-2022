b8bcb5e0a0b6297d62533ccf0c7d651955ad969f
John Kraal <john@johnkraal.com>
2022-12-01 11:10:41 +0100

    day01 improvement

--- days/day01.py
+++ days/day01.py
@@ -1,22 +1,26 @@
 #!/usr/bin/env python3
 
 
+def elf_generator(rd):
+    elf = []
+    for line in rd.readlines():
+        x = line.strip()
+        if x == "":
+            yield elf
+            elf = []
+            continue
+        elf.append(int(x))
+    yield elf
+
+
 def main():
     elves = []
     elvessums = []
     elf = []
     with open("../input/01.txt") as rd:
-        for line in rd.readlines():
-            x = line.strip()
-            if x == "":
-                elves.append(elf)
-                elvessums.append(sum(elf))
-                elf = []
-                continue
-
-            elf.append(int(x))
-        elvessums.append(sum(elf))
-        elves.append(elf)
+        for elf in elf_generator(rd):
+            elves.append(elf)
+            elvessums.append(sum(elf))
     elvessums = sorted(elvessums, reverse=True)
     print("p1:", elvessums[0])
     print("p2:", sum(elvessums[0:3]))
